c39c50b82fd742a7a964a86e899390a6560a7506
wizehack <wizehack@635d5333-5148-4f49-7e08-26cb3c2e26fd>
2013-10-27 06:49:50 +0000

    bug fix

--- trunk/mri/src/org/wizehack/mri/fl/DStar.java
+++ trunk/mri/src/org/wizehack/mri/fl/DStar.java
@@ -8,14 +8,18 @@ public class DStar extends LocalizationTechnique {
 
 	@Override
 	public double getSuspiciousness() {
-		int ncf = super.getFailed();
-		int nuf = super.getTotalFailed() - ncf;
-		int ncs = super.getPassed();
+		double ncf = super.getFailed();
+		double nuf = super.getTotalFailed() - ncf;
+		double ncs = super.getPassed();
 		double sus = 0;
-		int numerator = nuf + ncs;
-		if(numerator == 0)
-			sus = 0;
-		else
+		double numerator = nuf + ncs;
+		
+		if(numerator == 0){
+			if(ncf != 0 && ncf==nuf)
+				sus = 1;
+			else 
+				sus = 0;
+		} else
 			sus = (ncf * ncf) / (numerator);
 		return sus;
 	}
