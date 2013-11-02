f73e90fdbf97e40fded62eab5a94b02e188c452d
wizehack <wizehack@635d5333-5148-4f49-7e08-26cb3c2e26fd>
2013-11-02 05:51:21 +0000

    add additional method

--- trunk/mri/src/org/wizehack/mri/repo/DataManager.java
+++ trunk/mri/src/org/wizehack/mri/repo/DataManager.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.wizehack.mri.Test.TestCase;
+import org.wizehack.mri.io.FileIO;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
@@ -23,6 +24,16 @@ public class DataManager {
 	private static String STATISTICS = "statistics";
 	private static String FAULT_LOCALIZATION = "localization";
 	
+	public DataManager() {
+		mongoManager = MongoManager.getInstance();
+		mongoManager.createPool("127.0.0.1");
+	}
+	
+	public DataManager(String ip){
+		mongoManager = MongoManager.getInstance();
+		mongoManager.createPool(ip);
+	}
+	
 	public DataManager(String dbName, String ip){
 		this.DB_NAME = dbName;
 //		Configurator.getInstance().setProjectName(dbName);
@@ -39,8 +50,14 @@ public class DataManager {
 		} finally {
 			mongoManager.closeConnection(mongo);
 		}
-		
-		
+	}
+	
+	public Mongo getConnection(){
+		return mongoManager.getConnection();
+	}
+	
+	public void closeConnection(Mongo mongo){
+		mongoManager.closeConnection(mongo);
 	}
 	
 	public void insertTestResult(TestCase testCase){
@@ -324,5 +341,146 @@ public class DataManager {
 	
 	
 	
+	
+	public void exportToTextDoc(DB db, String expFile) {
+		List<Result> results = getLocalizationCollection(db);
+		List<String> doc = new ArrayList<String>();
+		FileIO fileIO = new FileIO();
+		fileIO.setOutFile(expFile);
+		
+		char seperator = '\t';
+		String column = "id" +seperator+ "file" +seperator+ "line"+seperator+
+				"suspciciousness"+seperator+"statement" ;
+		doc.add(column);
+		
+		for(int i=0; i<results.size(); i++) {
+			Result result = results.get(i);
+			String id = result.getStatementId();
+			String file = result.getFile();
+			String line = result.getLine();
+			String suspiciousness = result.getSuspiciousness();
+			String statement = result.getStatement().replace('\t', '\0');
+			
+			String fields = id + seperator + file + seperator + line + seperator + 
+					suspiciousness + seperator + statement;
+			
+			doc.add(fields);
+		}
+		fileIO.write(doc);
+	}
+	
+	public void updateSuspiciousness(DB db, double statementId, double suspiciousness){
+		DBCollection coll = db.getCollection(FAULT_LOCALIZATION);
+		
+		// get documents by query
+	    BasicDBObject query = new BasicDBObject("statementId", statementId);
+	 
+//	    cursor = coll.find(query);
+	     
+	  
+	    /**** Update ****/
+	    //update documents found by query 
+	    BasicDBObject newDocument = new BasicDBObject();
+	    newDocument.put("suspiciousness", suspiciousness);
+	  
+	    BasicDBObject updateObj = new BasicDBObject();
+	    updateObj.put("$set", newDocument);
+	  
+	    coll.update(query, updateObj,false,true);
+	}
+	
+	public boolean isCovered(DB db, double statementId){
+		DBCollection coll = db.getCollection(STATISTICS);
+		BasicDBObject query = new BasicDBObject("_id", statementId);
+		DBObject obj = coll.findOne(query);
+		
+		if(obj != null){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public static List<Spectrum> getStatistics(DB db) {
+		List<Spectrum> spectrumList = new ArrayList<Spectrum>();
+		DBCollection coll = db.getCollection(STATISTICS);
+		DBCursor cursor = coll.find();
+		
+		while(cursor.hasNext()){
+			DBObject dbObject = (DBObject) cursor.next();			
+			double statementId = (double) dbObject.get("_id");
+			DBObject value = (DBObject) dbObject.get("value");
+			double passed = (double) value.get("passed");
+			double failed = (double) value.get("failed");
+			Spectrum spectrum = new Spectrum((int)statementId, (int)passed, (int)failed);
+			spectrumList.add(spectrum);
+		}
+		return spectrumList;
+	}
+	
+	public static List<Code> getStatements(DB db) {
+		List<Code> codeList = new ArrayList<Code>();
+		DBCollection coll = db.getCollection(CODE);
+		DBCursor cursor = coll.find();
+		
+		while(cursor.hasNext()){
+			DBObject dbObject = (DBObject) cursor.next();		
+			Code code = new Code();
+			code.setFile(dbObject.get("file").toString());
+			code.setNumber((int)dbObject.get("lineNumber"));
+			try{
+				String statement = dbObject.get("statement").toString();
+				code.setStatement(statement);
+			} catch (NullPointerException ne){
+				code.setStatement("");
+			}
+			code.setStatementId((double)dbObject.get("statementId"));
+			codeList.add(code);
+		}
+		return codeList;
+	}
+	
+	public void insertSuspiciousness(DB db, Result result){
+		DBCollection coll = db.getCollection(FAULT_LOCALIZATION);
+		BasicDBObject query = new BasicDBObject();
+		
+		query.put( "statementId", result.getStatementId() );
+		query.put( "file", result.getFile() );
+		query.put( "line", result.getLine() );
+		query.put( "statement", result.getStatement());
+		query.put( "suspiciousness", result.getSuspiciousness());
+
+		coll.insert(query);
+	}
+	
+	public List<Result> getLocalizationCollection(DB db) {
+		List<Result> resultList = new ArrayList<Result>();
+		
+		DBCollection coll = db.getCollection(FAULT_LOCALIZATION);
+		DBCursor cursor = coll.find();
+		
+		while(cursor.hasNext()){
+			DBObject dbObject = (DBObject) cursor.next();		
+			
+			String statementId = dbObject.get("statementId").toString();
+			String file = dbObject.get("file").toString();
+			String line = dbObject.get("line").toString();
+			String statement = dbObject.get("statement").toString();
+			String suspiciousness = dbObject.get("suspiciousness").toString();
+			
+			Result result = new Result();
+			result.setStatementId(statementId);
+			result.setFile(file);
+			result.setLine(line);
+			result.setStatement(statement);
+			result.setSuspiciousness(suspiciousness);
+			
+			resultList.add(result);
+			
+		}
+		
+		return resultList;
+	}
+
 	
 }
