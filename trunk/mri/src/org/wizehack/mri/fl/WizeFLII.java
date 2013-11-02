afa957c7d2dc9d6849b626bdb943e9a6936f37a4
wizehack <wizehack@635d5333-5148-4f49-7e08-26cb3c2e26fd>
2013-11-02 09:02:23 +0000

    add wizeFLIII technique

---
+++ trunk/mri/src/org/wizehack/mri/fl/WizeFLII.java
@@ -0,0 +1,47 @@
+package org.wizehack.mri.fl;
+
+import java.util.List;
+
+import org.wizehack.mri.repo.Result;
+
+public class WizeFLII {
+	private LocalizationTechnique flTechnique = null;
+	private int numberOfGroups;
+	
+	public WizeFLII(String name, int numberOfGroups) {
+		super();
+//		this.flTechnique = flTechnique;
+		this.numberOfGroups = numberOfGroups;
+	}
+	
+	public double getSuspiciousness(int ith, boolean condition, double[] suspiciousnessGroup){
+		double suspiciousness = 0;
+		double sum=0;
+		if(condition){
+			
+			for(int i=0; i<suspiciousnessGroup.length; i++){
+				sum = sum + suspiciousnessGroup[i];
+			}
+			
+			suspiciousness = sum / suspiciousnessGroup.length;
+			
+		} else {
+			suspiciousness = suspiciousnessGroup[ith];
+		}
+		
+		return suspiciousness;
+	}
+	
+	public double getThreshold(List<Result> resultList){
+		double threshold = 0.0;
+		
+		double sum = 0;
+		for(int i=0; i<resultList.size(); i++){
+			sum = sum + new Double(resultList.get(i).getSuspiciousness());
+		}
+		
+		threshold = sum/resultList.size();
+		
+		return threshold;
+	}
+}
